c74c9f3af617b368bd5b2992fffc72e320e6ae45
whaugutin <whitchyaugustin@gmail.com>
2025-12-26 16:36:14 -0500

    Program.cs - Adding Menu and static method MenuRessources

--- Program.cs
+++ Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ReservationConsoleApp
+{
+    public class Program
+    {
+        private static ReservationService _service = new ReservationService();
+        static void Main(string[] args)
+        {
+                        bool quitter = false;
+            while (!quitter)
+            {
+                Console.Clear();
+                Console.WriteLine("=========================================");
+                Console.WriteLine("   SYSTÈME DE GESTION DE RÉSERVATIONS   ");
+                Console.WriteLine("=========================================");
+                Console.WriteLine("1. Consulter les ressources");
+                Console.WriteLine("2. Gérer les réservations");
+                Console.WriteLine("3. Afficher un récapitulatif de réservation");
+                Console.WriteLine("0. Quitter");
+                Console.WriteLine("=========================================");
+                Console.Write("Votre choix : ");
+                var choix = Console.ReadLine();
+
+                switch (choix)
+                {
+                    case "1":
+                        MenuRessources();
+                        break;
+                    case "2":
+                        MenuReservations();
+                        break;
+                    case "3":
+                        AfficherRecapReservation();
+                        break;
+                    case "0":
+                        quitter = true;
+                        break;
+                    default:
+                        Console.WriteLine("Choix invalide. Appuyez sur une touche pour continuer.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+
+        private static void MenuRessources()
+        {
+            Console.Clear();
+            Console.WriteLine("=== LISTE DES RESSOURCES ===");
+            foreach (var r in _service.GetResources())
+            {
+                Console.WriteLine(r);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Appuyez sur une touche pour revenir au menu principal.");
+            Console.ReadKey();
+        }
+
+    }
+}
