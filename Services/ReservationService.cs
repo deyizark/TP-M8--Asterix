a74502354154e8bce01218f75ddeac1017467a1a
Deyizark <mmomnsen@gmail.com>
2025-12-26 16:13:24 -0500

    Implementation fonction SeedData.ReservationService

--- Services/ReservationService.cs
+++ Services/ReservationService.cs
@@ -7,4 +7,21 @@ public class ReservationService
     private readonly List<Reservation> _reservations = new();
     private int _nextResourceId = 1;
     private int _nextReservationId = 1;
+
+     private void SeedData()
+        {
+            var managerSalle = new ResourceManager("Marie Laurent", "marie.laurent@entreprise.com");
+            var managerEq = new ResourceManager("Paul Martin", "paul.martin@entreprise.com");
+
+            _resources.Add(new Resource(_nextResourceId++, ResourceType.Salle, "Salle de réunion A", managerSalle));
+            _resources.Add(new Resource(_nextResourceId++, ResourceType.Salle, "Salle de réunion B", managerSalle));
+            _resources.Add(new Resource(_nextResourceId++, ResourceType.Chambre, "Chambre 101", managerSalle));
+            _resources.Add(new Resource(_nextResourceId++, ResourceType.Equipement, "Projecteur 1", managerEq));
+
+            var client = new Client("Jean Dupont", "jean.dupont@entreprise.com");
+            _clients.Add(client);
+
+            var reservation = new Reservation(_nextReservationId++, _resources[0], client, DateTime.Today);
+            _reservations.Add(reservation);
+        }
 }
