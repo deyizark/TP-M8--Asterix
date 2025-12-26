03d73b03c95d985a935d0e8a5df4759ff99bc726
Deyizark <mmomnsen@gmail.com>
2025-12-26 14:56:59 -0500

    Implementation classe ResourceManager

---
+++ Models/ResourceManager.cs
@@ -0,0 +1,10 @@
+namespace ReservationConsoleApp.Models
+{
+    public class ResourceManager : Person
+    {
+        public ResourceManager(string fullName, string email)
+            : base(fullName, email)
+        {
+        }
+    }
+}
