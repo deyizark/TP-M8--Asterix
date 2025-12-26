f9ed949d3eeac480af2393a56f81fc2bb73a5e22
whaugutin <whitchyaugustin@gmail.com>
2025-12-26 16:07:42 -0500

    Implematation de la classe ReservationService

--- Services/ReservationService.cs
+++ Services/ReservationService.cs
@@ -1,4 +1,10 @@
+using ReservationConsoleApp.Models;
+
 public class ReservationService
 {
-    // Implementation of ReservationService
+    private readonly List<Resource> _resources = new();
+    private readonly List<Client> _clients = new();
+    private readonly List<Reservation> _reservations = new();
+    private int _nextResourceId = 1;
+    private int _nextReservationId = 1;
 }
